dd8784e94467a123754f945c43317fc02bc35837
PanMoon1 <139225612+PanMoon1@users.noreply.github.com>
2024-01-15 11:08:38 +0300

    Add files via upload

---
+++ 8.3/8.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> spisok = new HashSet<int>();
+            int i = 0;
+            while (true)
+            {
+                i = int.Parse(Console.ReadLine());
+                if (spisok.Contains(i))
+                    Console.WriteLine("Такое число уже вводилось.");
+                else
+                {
+                    spisok.Add(i);
+                    Console.WriteLine("Число упешно сохранено.");
+                }
+            }
+        }
+    }
+}
