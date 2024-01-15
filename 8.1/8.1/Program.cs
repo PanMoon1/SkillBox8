87aa24ae32c2815a5d88674232801df523a8e48d
PanMoon1 <139225612+PanMoon1@users.noreply.github.com>
2024-01-15 11:07:42 +0300

    Add files via upload

---
+++ 8.1/8.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(GenerateList());
+
+            GetListOnScreen(list);
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+            list = DeleteSome(list);
+
+            GetListOnScreen(list);
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static List<int> GenerateList()
+        {
+            List<int> list = new List<int>();
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(r.Next(100));
+            }
+            return list;
+        }
+
+        static void GetListOnScreen(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i].ToString() + " ");
+            }
+        }
+
+        static List<int> DeleteSome(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < 50 && list[i] > 25)
+                    list.RemoveAt(i);
+            }
+            return list;
+        }
+    }
+}
