e1f49cfd046d3fdc2ceab2a6b0d7213b42d3ca93
PanMoon1 <139225612+PanMoon1@users.noreply.github.com>
2024-01-15 11:09:05 +0300

    Add files via upload

---
+++ 8.4/8.4/ClassContact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._4
+{
+    public class ClassContact
+    {
+        public string name;
+        public string street;
+        public int house;
+        public int flat;
+        public string mobilePhone;
+        public string flatPhone;
+
+        public ClassContact()
+        { }
+    }
+}
---
+++ 8.4/8.4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace _8._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"d:\Skill.xml";
+            ClassContact contact1 = new ClassContact();
+            CreateContact(contact1);
+
+            XElement contact =
+                new XElement("Person", contact1.name,
+                    new XElement("Adress",
+                        new XElement("Street", contact1.street),
+                        new XElement("HouseNumber", contact1.house.ToString()),
+                        new XElement("FlatNumber", contact1.flat.ToString())
+                        ),
+                    new XElement("Phones",
+                        new XElement("MobilePhone", contact1.mobilePhone),
+                        new XElement("FlatPhone", contact1.flatPhone)
+                        )
+                    );
+            
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                contact.Save(sw);
+                Console.WriteLine(contact);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void CreateContact(ClassContact c)
+        {
+            Console.WriteLine("Name");
+            c.name = Console.ReadLine();
+            Console.WriteLine("Street");
+            c.street = Console.ReadLine();
+            Console.WriteLine("House Number");
+            c.house = int.Parse(Console.ReadLine());
+            Console.WriteLine("Flat Number");
+            c.flat = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mobile Phone");
+            c.mobilePhone = Console.ReadLine();
+            Console.WriteLine("Flat Phone");
+            c.flatPhone = Console.ReadLine();
+        }
+    }
+}
